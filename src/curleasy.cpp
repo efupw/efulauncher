2a32ccc25a6a7c581cc791a4e44f842dcf31e75a
Mikkel Kjeldsen <commonquail@gmail.com>
2013-08-31 23:00:25 +0200

    Adds SSL certificate for GitHub and declares anonymous functions.
    This is a huge changeset, also a consequence of earlier mentioned whoopsie.
    Firstly it introduces an in-memory SSL certificate so curl can connect to
    GitHub. Secondly it adds a function, curlcheck(), which evaluates a
    CURLcode and throws an exception if the return value is not CURLE_OK.
    Thirdly it does a bit of stylistic cleanup.
    
    The reason this wasn't broken into smaller changesets is that add -p
    couldn't automatically split the hunks acceptably and I'm too lazy to do it
    manually.

--- src/curleasy.cpp
+++ src/curleasy.cpp
@@ -2,6 +2,10 @@
 #include "curleasy.h"
 #endif
 
+#ifndef HEADER_SSL_H
+#include "openssl/ssl.h"
+#endif
+
 #include <iostream>
 #include <iomanip>
 #include <sstream>
@@ -15,7 +19,24 @@ struct CurlEasy::progress_info
 
 namespace
 {
-    size_t writefunction(const char *ptr, size_t size, size_t nmemb, void *userdata)
+    size_t writefunction(const char *ptr, size_t size, size_t nmemb, void *userdata);
+    
+    const std::string sizetos(double size);
+
+    int progressfunction(void *p,
+            double dltotal, double dlnow,
+            double ultotal, double ulnow);
+
+    /**
+     * Read an SSL certificate from memory. The certificate is hardcoded.
+     * Because the first and third arguments are not used they have been left
+     * unnamed.
+     */
+    CURLcode sslctxfunction(CURL *, void *sslctx, void *);
+    
+    void curlcheck(CURLcode c);
+
+    size_t writefunction(const char * const ptr, const size_t size, const size_t nmemb, void * const userdata)
     {
         std::string *s = static_cast<std::string *>(userdata);
         s->append(ptr, size * nmemb);
@@ -42,9 +63,7 @@ namespace
         return ss.str();
     }
 
-    int progressfunction(void *p,
-            double dltotal, double dlnow,
-            double ultotal, double ulnow)
+    int progressfunction(void *p, double dltotal, double dlnow, double, double)
     {
         CurlEasy::progress_info *progress
             = static_cast<CurlEasy::progress_info *>(p);
@@ -93,8 +112,64 @@ namespace
 
         return 0;
     }
-}
+    
+    /*
+     * DigiCert High Assurance EV Root CA certificate in PEM format taken from
+     * http://curl.haxx.se/docs/caextract.html
+    */
+    const std::string certificate(
+        "-----BEGIN CERTIFICATE-----\n"\
+        "MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBsMQswCQYDVQQG\n"\
+        "EwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMSsw\n"\
+        "KQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5jZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAw\n"\
+        "MFoXDTMxMTExMDAwMDAwMFowbDELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZ\n"\
+        "MBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFu\n"\
+        "Y2UgRVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm+9S75S0t\n"\
+        "Mqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTWPNt0OKRKzE0lgvdKpVMS\n"\
+        "OO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEMxChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3\n"\
+        "MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFBIk5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQ\n"\
+        "NAQTXKFx01p8VdteZOE3hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUe\n"\
+        "h10aUAsgEsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMB\n"\
+        "Af8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaAFLE+w2kD+L9HAdSY\n"\
+        "JhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3NecnzyIZgYIVyHbIUf4KmeqvxgydkAQ\n"\
+        "V8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6zeM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFp\n"\
+        "myPInngiK3BD41VHMWEZ71jFhS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkK\n"\
+        "mNEVX58Svnw2Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe\n"\
+        "vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep+OkuE6N36B9K\n"\
+        "-----END CERTIFICATE-----\n");
 
+    CURLcode sslctxfunction(CURL * const, void * const sslctx, void * const)
+    {
+        BIO * const bio = BIO_new_mem_buf(const_cast<char *>(certificate.c_str()),
+            certificate.length());
+        /*
+        Read the PEM formatted certificate from memory into an X509 struct that
+        SSL can use.
+        */ 
+        X509 * const cert = PEM_read_bio_X509(bio, nullptr, 0, nullptr);
+        if (!cert)
+        {
+            return CURLE_SSL_CERTPROBLEM;
+        }
+
+        X509_STORE * const store = SSL_CTX_get_cert_store(static_cast<const SSL_CTX *>(sslctx));
+        if (X509_STORE_add_cert(store, cert) == 0)
+        {
+            return CURLE_SSL_CERTPROBLEM;
+        }
+
+        return CURLE_OK ;
+    }
+    
+    inline void curlcheck(const CURLcode c)
+    {
+        if (c != CURLE_OK)
+        {
+            throw CurlEasyException(c);
+        }
+    }
+}
+        
 CurlGlobalInit::CurlGlobalInit()
 {
     curl_global_init(CURL_GLOBAL_ALL);
@@ -120,11 +195,15 @@ CurlEasy::CurlEasy(const std::string &url):
         throw CurlEasyException("Empty URL.");
     }
 
-    CURLcode c = curl_easy_setopt(*m_pcurl, CURLOPT_URL, url.c_str());
-    if (c != CURLE_OK)
-    {
-        throw CurlEasyException(c);
-    }
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_URL, url.c_str()));
+
+    // For hosts, 2 means verify, 0 means don't, and 1 is ignored!
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_SSL_VERIFYHOST, 2L));
+
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_SSL_VERIFYPEER, 1L));
+    
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_SSL_CTX_FUNCTION,
+        &sslctxfunction));
 }
 
 CurlEasy::~CurlEasy()
@@ -142,42 +221,24 @@ void CurlEasy::perform()
         throw CurlEasyException("Cannot reuse curl handles.");
     }
 
-    CURLcode c = curl_easy_perform(*m_pcurl);
-    if (c != CURLE_OK)
-    {
-        throw CurlEasyException(c);
-    }
-    else
-    {
-        std::cout << "\n";
-        m_used = true;
-    }
+    curlcheck(curl_easy_perform(*m_pcurl));
+    std::cout << "\n";
+    m_used = true;
 }
 
 void CurlEasy::write_to(const std::string &dest)
 {
-    CURLcode c = curl_easy_setopt(*m_pcurl,
-            CURLOPT_WRITEFUNCTION, &writefunction);
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_WRITEFUNCTION,
+        &writefunction));
 
-    if (c != CURLE_OK)
-    {
-        throw CurlEasyException(c);
-    }
-
-    c = curl_easy_setopt(*m_pcurl,
-            CURLOPT_WRITEDATA, &dest);
-
-    if (c != CURLE_OK)
-    {
-        throw CurlEasyException(c);
-    }
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_WRITEDATA, &dest));
 }
 
 void CurlEasy::progressbar(bool val)
 {
     if (!val)
     {
-        curl_easy_setopt(*m_pcurl, CURLOPT_NOPROGRESS, 1L);
+        curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_NOPROGRESS, 1L));
         return;
     }
 
@@ -186,7 +247,9 @@ void CurlEasy::progressbar(bool val)
     m_progress->sdltotal = "";
     m_progress->curl = *m_pcurl;
 
-    curl_easy_setopt(*m_pcurl, CURLOPT_NOPROGRESS, 0L);
-    curl_easy_setopt(*m_pcurl, CURLOPT_PROGRESSFUNCTION, &progressfunction);
-    curl_easy_setopt(*m_pcurl, CURLOPT_PROGRESSDATA, m_progress.get());
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_NOPROGRESS, 0L));
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_PROGRESSFUNCTION,
+        &progressfunction));
+    curlcheck(curl_easy_setopt(*m_pcurl, CURLOPT_PROGRESSDATA,
+        m_progress.get()));
 }
