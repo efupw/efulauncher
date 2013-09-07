2ca2a0c0ba85a858641314ac9a6f3f090593b45c
Mikkel Kjeldsen <commonquail@gmail.com>
2013-09-07 12:24:39 +0200

    Move SimpleReadHKLMKey to separate file.

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,16 @@
 #include "curleasy.h"
 #endif
 
-#if defined(_WIN32) && !defined(EFU_WINERRORSTRING_H)
+#ifdef _WIN32
+
+#ifndef EFU_WINERRORSTRING_H
 #include "win_error_string.hpp"
+#endif
+
+#ifndef EFU_SIMPLEREADHKLMKEY_H
+#include "simple_read_hklm_key.h"
+#endif
+
 #endif
 
 const std::string version("0.1.0");
@@ -462,54 +470,6 @@ class EfuLauncher
         const std::string m_update_check;
         std::string m_update_path;
         bool m_has_update;
-};
-
-class SimpleReadHKLMKey
-{
-public:
-    explicit SimpleReadHKLMKey(const std::string &location, const std::string &name):
-        m_hkey(nullptr),
-        m_val()
-    {
-        LONG ret = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE,
-            location.c_str(),
-            0, KEY_READ | KEY_WOW64_32KEY, &m_hkey);
-        if (ret == ERROR_SUCCESS)
-        {
-            CHAR szBuffer[128];
-            DWORD dwBufferSize = sizeof(szBuffer);
-            ret = ::RegQueryValueEx(m_hkey,
-                name.c_str(),
-                0, nullptr,
-                reinterpret_cast<LPBYTE>(szBuffer),
-                &dwBufferSize);
-            
-            // RegQueryValueEx() does not guarantee the string is
-            // null-terminated.
-            szBuffer[dwBufferSize - 1] = '\0';
-            
-            if (ret == ERROR_SUCCESS)
-            {
-                m_val = szBuffer;
-            }
-        }
-    }
-    
-    const std::string str() const { return m_val; }
-    
-    ~SimpleReadHKLMKey()
-    {
-        if (m_hkey)
-        {
-            ::RegCloseKey(m_hkey);
-        }
-    }
-    
-    bool good() const { return !m_val.empty(); }
-    
-private:
-    HKEY m_hkey;
-    std::string m_val;
 };
 
 int main(int, char *argv[])
