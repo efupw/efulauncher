d3e56da0d7985cfe64139b50a781520631b6b79c
Mikkel Kjeldsen <commonquail@gmail.com>
2013-09-21 13:00:17 +0200

    If continuing without NWN root dir, set to ./

--- src/main.cpp
+++ src/main.cpp
@@ -124,6 +124,7 @@ int main(int argc, char *argv[])
             std::cout << "Exiting EfU Launcher. Goodbye!" << std::endl;
             return 0;
         }
+        nwn_root_dir = "./";
     }
 #endif
 
